90fd3360146881e81e1ab1c2759844d868ccd605
Petr Majner <petr@majner.cz>
2026-05-31 20:04:23 +0000

    Aktualizovat custom_components/avtech_mjpeg_dvr/config_flow.py

--- custom_components/avtech_mjpeg_dvr/config_flow.py
+++ custom_components/avtech_mjpeg_dvr/config_flow.py
@@ -1,35 +1,19 @@
+"""Triviální config flow pro testovací pruhovanou kameru."""
 import voluptuous as vol
 from homeassistant import config_entries
 
 DOMAIN = "avtech_mjpeg_dvr"
 
-class AvtechDVRConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Grafické nastavení pro AV-TECH MJPEG DVR."""
-
+class AvtechTrivialConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Triviální grafické nastavení."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Zobrazení formuláře uživateli v rozhraní HA."""
-        errors = {}
-
+        """Zobrazení jednoduchého formuláře."""
         if user_input is not None:
-            await self.async_set_unique_id(f"avtech_{user_input['ip']}_{user_input['port']}")
-            self._abort_if_unique_id_configured()
-
-            return self.async_create_entry(
-                title=f"AV-TECH DVR ({user_input['ip']})", 
-                data=user_input
-            )
-
-        DATA_SCHEMA = vol.Schema(
-            {
-                vol.Required("ip", default="192.168.1.20"): str,
-                vol.Required("port", default=8888): int,
-                vol.Required("username", default="admin"): str,
-                vol.Required("password"): str,
-            }
-        )
+            return self.async_create_entry(title="AV-TECH Pruhy Test", data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=vol.Schema({vol.Required("ip", default="192.168.2.10"): str})
         )
