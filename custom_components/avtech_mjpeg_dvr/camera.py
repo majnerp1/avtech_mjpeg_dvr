eba096a2a46edb589cf4a59e1dbc98afaab313b9
Petr Majner <petr@majner.cz>
2026-05-31 21:44:51 +0000

    Aktualizovat custom_components/avtech_mjpeg_dvr/camera.py

--- custom_components/avtech_mjpeg_dvr/camera.py
+++ custom_components/avtech_mjpeg_dvr/camera.py
@@ -1,32 +1,42 @@
-"""Generátor testovacího pruhovaného obrazu pro ostrou doménu."""
+"""AV-TECH testovací kamera (pruhovaný obraz)."""
+
 import logging
 from homeassistant.components.camera import Camera
 
 _LOGGER = logging.getLogger(__name__)
+
 DOMAIN = "avtech_mjpeg_dvr"
 
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Nastavení testovací kamery."""
-    async_add_entities([AvtechOstraCamera()])
-    return True
+    """Setup camera platform from config entry."""
+    async_add_entities([AvtechOstraCamera(config_entry)])
+
 
 class AvtechOstraCamera(Camera):
-    """Kamera generující pevný pruhovaný vzor."""
+    """Jednoduchá testovací kamera s generovaným obrazem."""
 
-    def __init__(self):
+    def __init__(self, config_entry):
         """Inicializace."""
         super().__init__()
-        self._attr_name = "AV-TECH Ostré Pruhy"
+        self.config_entry = config_entry
 
-    @property
-    def name(self):
-        return self._attr_name
+        self._attr_name = "AV-TECH Ostré Pruhy"
+        self._attr_unique_id = f"{config_entry.entry_id}_ostra_camera"
 
     async def async_camera_image(self, width=None, height=None):
-        """Vrací binární JPEG s černobílými pruhy."""
-        return b"".join([
-            b"\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x01\x00H\x00H\x00\x00\xff\xdb\x00C\x00\x08\x06\x06\x07\x06\x05\x08\x07\x07\x07\t\t\x08\n\x0c\x14\r\x0c\x0b\x0b\x0c\x19\x12\x13\x0f\x14\x1d\x1a\x1f\x1e\x1d\x1a\x1c\x1c $.' \",#\x1c\x1c(7),01444\x1f'9=82<.342\xff\xc0\x00\x0b\x08\x00\x10\x00\x10\x01\x01\x11\x00\xff\xc4\x00\x1f\x00\x00\x01\x05\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\xff\xda\x00\x08\x01\x01\x00\x00?\x00\xf5\x7f\x80",
-            b"\x55" * 128,
-            b"\xaa" * 128,
-            b"\xff\xd9"
-        ])
+        """Vrací statický JPEG obraz (test pattern)."""
+        return b"".join(
+            [
+                b"\xff\xd8\xff\xe0\x00\x10JFIF",
+                b"\x00" * 64,
+                b"\x11\x22\x33\x44" * 32,
+                b"\xaa" * 128,
+                b"\xff\xd9",
+            ]
+        )
+
+    @property
+    def supported_features(self):
+        """Required property in newer HA versions."""
+        return 0
