2a0924245379d708e04b3c7d1d5ed98cec8b3c2e
Petr Majner <petr@majner.cz>
2026-05-31 20:15:36 +0000

    Aktualizovat custom_components/avtech_mjpeg_dvr/config_flow.py

--- custom_components/avtech_mjpeg_dvr/config_flow.py
+++ custom_components/avtech_mjpeg_dvr/config_flow.py
@@ -1,17 +1,17 @@
-"""Triviální config flow pro testovací pruhovanou kameru."""
+"""Triviální config flow pro AV-TECH."""
 import voluptuous as vol
 from homeassistant import config_entries
 
 DOMAIN = "avtech_mjpeg_dvr"
 
-class AvtechTrivialConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class AvtechDVRConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Triviální grafické nastavení."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         """Zobrazení jednoduchého formuláře."""
         if user_input is not None:
-            return self.async_create_entry(title="AV-TECH Pruhy Test", data=user_input)
+            return self.async_create_entry(title="AV-TECH Pruhy Ostré", data=user_input)
 
         return self.async_show_form(
             step_id="user",
