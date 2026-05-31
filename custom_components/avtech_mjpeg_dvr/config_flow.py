2a3f936938cd56e7d7ce1199672876a7c7f3550a
Petr Majner <petr@majner.cz>
2026-05-31 21:49:19 +0000

    Aktualizovat custom_components/avtech_mjpeg_dvr/config_flow.py

--- custom_components/avtech_mjpeg_dvr/config_flow.py
+++ custom_components/avtech_mjpeg_dvr/config_flow.py
@@ -1,19 +1,29 @@
-"""Triviální config flow pro AV-TECH."""
+"""Config flow for AV-TECH MJPEG DVR."""
 import voluptuous as vol
+
 from homeassistant import config_entries
 
-DOMAIN = "avtech_mjpeg_dvr"
+from .const import DOMAIN
 
-class AvtechDVRConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Triviální grafické nastavení."""
+
+class AvtechConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Simple config flow."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Zobrazení jednoduchého formuláře."""
+        """User setup step."""
         if user_input is not None:
-            return self.async_create_entry(title="AV-TECH Pruhy Ostré", data=user_input)
+            return self.async_create_entry(
+                title="AV-TECH MJPEG DVR",
+                data=user_input,
+            )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required("ip", default="192.168.2.10"): str})
+            data_schema=vol.Schema(
+                {
+                    vol.Required("ip", default="192.168.1.10"): str,
+                }
+            ),
         )
